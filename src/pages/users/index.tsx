45ed5236a0b1ceb3a33e9fc1e7ad5ad59693aad0
Pedro Henrique <pedrops02@gmail.com>
2022-01-12 17:46:33 -0300

    adicao de paginas de listagem e criacao de usuario

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,86 @@
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function userList(){
+    return (
+        <Box>
+            <Header />
+
+            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+                <Sidebar />
+
+                <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+                    <Flex mb="8" justify="space-between" align="center">
+                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+                        <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon fontSize="20" as={RiAddLine}/>} >
+                            Criar novo
+                        </Button>
+
+                    </Flex>
+
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink" />
+                                </Th>
+                                <Th>Usuário</Th>
+                                <Th>Data de cadastro</Th>
+                                <Th width="6"></Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td pd="6">
+                                    <Checkbox colorScheme="pink" />
+                                </Td>
+                                <Td>
+                                    <Box>
+                                        <Text fontWeight="bold">Pedro Teixeira</Text>
+                                        <Text fontSize="sm" color="gray.300">pedrops02@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                                <Td>01 de Abril, 2022</Td>
+                                <Td>
+                                    <Button as="a" size="sm" fontSize="sm"  colorScheme="purple" leftIcon={<Icon fontSize="16" as={RiPencilLine}/>} >
+                                        Editar
+                                    </Button>
+                                </Td>
+
+                            </Tr>
+                        </Tbody>
+                    </Table>
+                    <Pagination />
+                </Box>
+
+            </Flex>
+        </Box>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    )
+}
